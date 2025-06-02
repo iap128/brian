5fe644be520f5903da2cba136d9512e15b3b6df1
iap128 <103337777+iap128@users.noreply.github.com>
2025-06-02 14:54:04 -0700

    Updated Gemini version

--- src/ChatContext.tsx
+++ src/ChatContext.tsx
@@ -34,7 +34,7 @@ const ChatProvider: FC<Props> = ({ children }) => {
   //create a file called `config.ts` in /src and export
   //a variable called `apiKey` that contains your own key
   const genAI = new GoogleGenerativeAI(apiKey);
-  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+  const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 
   useEffect(() => {
     scrollToBottom(divRef);
