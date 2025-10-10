c4376c6e0efa79e7275bc0c4362a5914bc431574
iap128 <103337777+iap128@users.noreply.github.com>
2025-10-09 17:29:25 -0700

    Redesign (#4)
    * Redesign UI
    
    * Added copy feature
    
    * Removed upload for now
    
    ---------
    
    Co-authored-by: Ryan Hunter <Ryan.Hunter@nike.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Chat from './chatUI/Chat';
 import './App.css';
 import { PlusOutlined } from '@ant-design/icons';
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <Layout className='layout' style={{ backgroundColor: 'transparent' }}>
+    <Layout className="layout" style={{ backgroundColor: 'transparent' }}>
       <Header
         style={{
           display: 'flex',
@@ -21,38 +21,18 @@ function App() {
           boxShadow: '0 8px 32px 0 lightgray',
         }}
       >
-        <Typography.Title>Ask Brian</Typography.Title>
-        <Button icon={<PlusOutlined />} type='dashed' onClick={() => window.location.reload()}>
+        <Typography.Title level={3}>Ask Brian</Typography.Title>
+
+        <Button icon={<PlusOutlined />} type="dashed" onClick={() => window.location.reload()}>
           New Chat
         </Button>
       </Header>
 
       <Content style={{ padding: '10px 5%' }}>
-        <div
-          className='site-layout-content'
-          style={{
-            borderRadius: 10,
-            background: 'white',
-            boxShadow: '0 8px 32px 0 lightgray',
-            height: '75vh',
-          }}
-        >
+        <div className="site-layout-content">
           <Chat />
         </div>
       </Content>
-
-      <Footer
-        style={{
-          textAlign: 'center',
-          background: 'white',
-          boxShadow: '0 8px 32px 0 lightgray',
-        }}
-      >
-        Made with ❤️ by{' '}
-        <Button type='dashed' href='https://n818pe.com'>
-          Ryan Hunter
-        </Button>
-      </Footer>
     </Layout>
   );
 }
